82f9c6750663f643eea4ffaf7b0ee998cd08c452
Alexander Winston <34994061+axwn@users.noreply.github.com>
2020-06-12 18:25:17 -0400

    Add zoning districts (cleaned with mapshaper)

---
+++ rzd.js
@@ -0,0 +1,360 @@
+const zoning_districts = {
+  "CM": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": "",
+    "housing_allowed": false,
+    "two_family_allowed": false,
+    "multi_family_allowed": false
+  },
+  "DCC": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": "",
+    "housing_allowed": false,
+    "two_family_allowed": false,
+    "multi_family_allowed": false
+  },
+  "M-1": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": "",
+    "housing_allowed": false,
+    "two_family_allowed": false,
+    "multi_family_allowed": false
+  },
+  "M-2": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": "",
+    "housing_allowed": false,
+    "two_family_allowed": false,
+    "multi_family_allowed": false
+  },
+  "OS": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": "",
+    "housing_allowed": false,
+    "two_family_allowed": false,
+    "multi_family_allowed": false
+  },
+  "RP": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": "",
+    "housing_allowed": false,
+    "two_family_allowed": false,
+    "multi_family_allowed": false
+  },
+  "B-1": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 87,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "B-2": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 130,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "B2-POD": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 130,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "B-3": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 87,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "B-4": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 261,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "B-5": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 217,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "B-6": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 174,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "B-7": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 217,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "HO": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": 130,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "I": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": "",
+    "housing_allowed": "TRUE",
+    "two_family_allowed": false,
+    "multi_family_allowed": false
+  },
+  "R-1": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": "",
+    "housing_allowed": "TRUE",
+    "two_family_allowed": false,
+    "multi_family_allowed": false
+  },
+  "R-2": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": "",
+    "housing_allowed": "TRUE",
+    "two_family_allowed": false,
+    "multi_family_allowed": false
+  },
+  "R-3": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": "",
+    "housing_allowed": "TRUE",
+    "two_family_allowed": false,
+    "multi_family_allowed": false
+  },
+  "R-4": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": "",
+    "housing_allowed": "TRUE",
+    "two_family_allowed": false,
+    "multi_family_allowed": false
+  },
+  "R-5": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": "",
+    "housing_allowed": "TRUE",
+    "two_family_allowed": false,
+    "multi_family_allowed": false
+  },
+  "R-6": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": "",
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": false
+  },
+  "R-7": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": "",
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": false
+  },
+  "R-8": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": "",
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": false
+  },
+  "R-43": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": 14,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "R-48": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": 19,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "R-53": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": 34,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "R-63": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": 43,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "R-73": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": 87,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "R-MH": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": "",
+    "housing_allowed": "TRUE",
+    "two_family_allowed": false,
+    "multi_family_allowed": false,
+    "density_limit": "8 DU per acre"
+  },
+  "RF-1": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": 261,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "RF-2": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": 566,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "RO-1": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": 14,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "RO-2": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": 34,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "RO2-PE2": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": 34,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "RO2-PE5": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": 34,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "RO-3": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": 130,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "TOD-1": {
+    "grocery_store_allowed": "",
+    "max_units_per_acre": 522,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "UB": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 87,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "UB-PE2": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 87,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "UB-PE3": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 87,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "UB-PE4": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 87,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "UB-PE5": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 87,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "UB-PE6": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 87,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "UB-PE7": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 87,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "UB-PO1": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 87,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "UB-PO2": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 87,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "UB-PO3": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 87,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "UB-PO4": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 87,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "UB-2": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 130,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  },
+  "UB2-PE8": {
+    "grocery_store_allowed": "TRUE",
+    "max_units_per_acre": 130,
+    "housing_allowed": "TRUE",
+    "two_family_allowed": "TRUE",
+    "multi_family_allowed": "TRUE"
+  }
+}
